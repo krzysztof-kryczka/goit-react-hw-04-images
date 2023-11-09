9324eaa54e449bb2be5ede8fa0a8cfb8f177a970
Krzysztof Kryczka <kw.kryczka@gmail.com>
2023-11-09 19:13:19 +0100

    Refactor - component Button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,17 +1,13 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Button.module.css';
 
-export class Button extends Component {
-  render() {
-    const { onClick } = this.props;
+const Button = ({onClick}) => {
 
     return (
       <button type="button" onClick={onClick} className={styles.Button}>
         Load more
       </button>
     );
-  }
 }
 
 Button.propTypes = {
