508f4c781c6f87b14c0b2eb5579767115f837039
Krzysztof Kryczka <kw.kryczka@gmail.com>
2023-11-09 19:17:39 +0100

    Refactor - component ImageGallery and ImageGalleryItem

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,27 +1,22 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './ImageGallery.module.css';
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 
-export class ImageGallery extends Component {
-  render() {
-    const { cards, onShow } = this.props;
-
-    return (
-      <ul className={styles.ImageGallery}>
-        {cards.map(({ webformatURL, largeImageURL, id, tags }) => (
-          <ImageGalleryItem
-            key={id}
-            webformatURL={webformatURL}
-            largeImageURL={largeImageURL}
-            onShow={onShow}
-            tags={tags}
-          />
-        ))}
-      </ul>
-    );
-  }
-}
+const ImageGallery = ({ cards, onShow }) => {
+  return (
+    <ul className={styles.ImageGallery}>
+      {cards.map(({ webformatURL, largeImageURL, id, tags }) => (
+        <ImageGalleryItem
+          key={id}
+          webformatURL={webformatURL}
+          largeImageURL={largeImageURL}
+          onShow={onShow}
+          tags={tags}
+        />
+      ))}
+    </ul>
+  );
+};
 
 ImageGallery.propTypes = {
   onShow: PropTypes.func.isRequired,
--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,23 +1,18 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './ImageGalleryItem.module.css';
 
-export class ImageGalleryItem extends Component {
-  render() {
-    const { webformatURL, largeImageURL, onShow, tags } = this.props;
-
-    return (
-      <li className={styles.galleryItem}>
-        <img
-          className={styles.ImageGalleryItemImage}
-          onClick={() => onShow(largeImageURL)}
-          src={webformatURL}
-          alt={tags}
-        />
-      </li>
-    );
-  }
-}
+const ImageGalleryItem = ({ webformatURL, largeImageURL, onShow, tags }) => {
+  return (
+    <li className={styles.galleryItem}>
+      <img
+        className={styles.ImageGalleryItemImage}
+        onClick={() => onShow(largeImageURL)}
+        src={webformatURL}
+        alt={tags}
+      />
+    </li>
+  );
+};
 
 ImageGalleryItem.propTypes = {
   onShow: PropTypes.func,
