c5872065c872d37b4ae97a6375b22b054a517562
Krzysztof Kryczka <kw.kryczka@gmail.com>
2023-11-10 10:24:30 +0100

    Refactor - using React-hooks useState and useEffect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { fetchPhoto } from '../services/pixabay-api';
 import Searchbar from './SearchBar/SearchBar';
 import ImageGallery from './ImageGallery/ImageGallery';
@@ -7,83 +7,77 @@ import Button from './Button/Button';
 import Modal from './Modal/Modal';
 import Notiflix from 'notiflix';
 
-class App extends Component {
-  state = {
-    search: '',
-    currentPage: 0,
-    totalHits: 0,
-    images: [],
-    error: null,
-    isLoading: false,
-    isModalOpen: false,
-    largeImageURL: '',
-    webformatURL: '',
+const App = () => {
+  const [query, setQuery] = useState('');
+  const [error, setError] = useState(null);
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [largeImageURL, setLargeImageURL] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalHits, setTotalHits] = useState(0);
+
+  const handleSubmit = search => {
+    setCurrentPage(1);
+    setImages([]);
+    setQuery(search);
   };
 
-  handleSubmit = search => {
-    if (search !== this.state.search) {
-      this.setState({ images: [], currentPage: 1, search }, () => {
-        this.fetchSearch(search);
-      });
-    }
+  const handleButton = () => {
+    setCurrentPage(prev => prev + 1);
   };
 
-  handleButton = () => {
-    this.setState({ currentPage: this.state.currentPage + 1 }, () => {
-      this.fetchSearch(this.state.search);
-    });
-  };
+  useEffect(() => {
+    if (!query) return;
 
-  fetchSearch = async valueSearch => {
-    this.setState({ isLoading: true, error: null });
+    fetchSearch(query, currentPage);
+  }, [query, currentPage]);
+
+  const fetchSearch = async (valueSearch, valueCurrentPage) => {
+    setIsLoading(true);
+    setError(null);
     try {
-      const response = await fetchPhoto(valueSearch, this.state.currentPage);
+      const response = await fetchPhoto(valueSearch, valueCurrentPage);
 
       if (response.hits.length === 0) {
         Notiflix.Notify.failure(
           'Sorry, there are no images matching your search query. Please try again.'
         );
-        this.setState({ currentPage: 0 });
         return;
       }
-
-      this.setState(prevState => ({
-        images: [...prevState.images, ...response.hits],
-        totalHits: response.totalHits,
-      }));
+      setTotalHits(response.totalHits);
+      setImages(prevState => [...prevState, ...response.hits]);
     } catch (error) {
-      this.setState({ error: error });
+      setError(error);
     } finally {
-      setTimeout(() => this.setState({ isLoading: false }), 600);
+      setTimeout(() => setIsLoading(false), 600);
     }
   };
 
-  handleModalClose = () => {
-    this.setState({ isModalOpen: false, largeImageURL: '' });
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+    setLargeImageURL('');
   };
 
-  handleModalOpen = url => {
-    this.setState({ isModalOpen: true, largeImageURL: url });
+  const handleModalOpen = url => {
+    setIsModalOpen(true);
+    setLargeImageURL(url);
   };
 
-  render() {
-    const { images, largeImageURL, totalHits, isLoading, isModalOpen } =
-      this.state;
-    return (
-      <div>
-        <Searchbar onSubmit={this.handleSubmit} />
-        <ImageGallery cards={images} onShow={this.handleModalOpen} />
-        {isLoading && <Loader />}
-        {images.length !== 0 && images.length !== totalHits && (
-          <Button onClick={this.handleButton} />
-        )}
-
-        {isModalOpen && (
-          <Modal onClose={this.handleModalClose} image={largeImageURL} />
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      {error && <p>Something went wrong: {error.message}</p>}
+      <Searchbar onSubmit={handleSubmit} />
+      <ImageGallery cards={images} onShow={handleModalOpen} />
+      {isLoading && <Loader />}
+      {images.length !== 0 && images.length !== totalHits && (
+        <Button onClick={handleButton} />
+      )}
+      {isModalOpen && (
+        <Modal onClose={handleModalClose} image={largeImageURL} />
+      )}
+    </div>
+  );
+};
 
 export default App;
